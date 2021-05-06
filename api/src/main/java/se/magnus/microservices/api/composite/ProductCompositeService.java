40e4579878a98cb80d6fb52cb6716213f40c21ce
cirovladimir <cirovladimir@gmail.com>
2021-05-05 23:58:23 -0500

    add springfox to generate swagger REST API documentation

--- api/src/main/java/se/magnus/microservices/api/composite/ProductCompositeService.java
+++ api/src/main/java/se/magnus/microservices/api/composite/ProductCompositeService.java
@@ -3,8 +3,20 @@ package se.magnus.microservices.api.composite;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(description = "REST API for composite product information")
 public interface ProductCompositeService {
     
     @GetMapping(value = "/product-composite/{productId}", produces = "application/json")
+    @ApiOperation(value = "${api.product-composite.get.description}", notes = "${api.product-composite.get.notes}")
+    @ApiResponses(value = {
+        @ApiResponse(code = 400, message = "Bad Request, invalid format of the request. See response message for more information."),
+        @ApiResponse(code = 404, message = "Not Found, the specified id does not exist."),
+        @ApiResponse(code = 422, message = "Unprocessable entity, input parameters caused the processing to fail. See response message for more information."),
+    })
     ProductAggregate getProduct(@PathVariable(value = "productId", required = true)int productId);
 }
--- product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
+++ product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -1,10 +1,21 @@
 package se.magnus.microservices.composite.product;
 
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 @ComponentScan("se.magnus")
@@ -14,9 +25,47 @@ public class ProductCompositeServiceApplication {
 		SpringApplication.run(ProductCompositeServiceApplication.class, args);
 	}
 
+	@Value("${api.common.title}")
+	private String apiTitle;
+	@Value("${api.common.description}")
+	private String apiDescription;
+	@Value("${api.common.version}")
+	private String apiVersion;
+	@Value("${api.common.termsOfServiceUrl}")
+	private String apiTermsOfServiceUrl;
+	@Value("${api.common.contact.name}")
+	private String apiContactName;
+	@Value("${api.common.contact.url}")
+	private String apiContactUrl;
+	@Value("${api.common.contact.email}")
+	private String apiContactEmail;
+	@Value("${api.common.license}")
+	private String apiLicense;
+	@Value("${api.common.licenseUrl}")
+	private String apiLicenseUrl;
+
 	@Bean
 	RestTemplate restTemplate(){
 		return new RestTemplate();
+	}
+
+	@Bean
+	Docket docket(){
+		return new Docket(DocumentationType.SWAGGER_2)
+		.select()
+		.apis(RequestHandlerSelectors.basePackage("se.magnus.microservices.composite.product"))
+		.paths(PathSelectors.any())
+		.build()
+		.globalResponseMessage(RequestMethod.GET, Collections.emptyList())
+		.apiInfo(new ApiInfo(
+			apiTitle, 
+			apiDescription, 
+			apiVersion, 
+			apiTermsOfServiceUrl, 
+			new Contact(apiContactName, apiContactUrl, apiContactEmail), 
+			apiLicense, 
+			apiLicenseUrl, 
+			Collections.emptyList()));
 	}
 
 }
