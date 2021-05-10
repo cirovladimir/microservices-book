b6ceca733ca7960bd6b860a404bb267469e54f58
cirovladimir <cirovladimir@gmail.com>
2021-05-10 10:08:50 -0500

    add test for duplicate key exception

--- product-service/src/test/java/se/magnus/microservices/core/product/persistence/ProductRepositoryTest.java
+++ product-service/src/test/java/se/magnus/microservices/core/product/persistence/ProductRepositoryTest.java
@@ -1,31 +1,33 @@
 package se.magnus.microservices.core.product.persistence;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.dao.DuplicateKeyException;
 
 @DataMongoTest
 public class ProductRepositoryTest {
-    
+
     @Autowired
     ProductRepository productRepository;
 
     ProductEntity savedEntity;
 
     @BeforeEach
-    public void setup(){
+    public void setup() {
         productRepository.deleteAll();
-        ProductEntity productEntity = new ProductEntity(1,"product-1", 1);
+        ProductEntity productEntity = new ProductEntity(1, "product-1", 1);
         savedEntity = productRepository.save(productEntity);
         assertEquals(productEntity, savedEntity);
     }
 
     @Test
-    public void create(){
-        ProductEntity newEntity = new ProductEntity(2,"product-2", 2);
+    public void create() {
+        ProductEntity newEntity = new ProductEntity(2, "product-2", 2);
         productRepository.save(newEntity);
 
         ProductEntity foundEntity = productRepository.findById(newEntity.getId()).get();
@@ -35,13 +37,21 @@ public class ProductRepositoryTest {
     }
 
     @Test
-    public void update(){
+    public void update() {
         savedEntity.setName("product-updated");
         productRepository.save(savedEntity);
 
         ProductEntity foundEntity = productRepository.findById(savedEntity.getId()).get();
 
         assertEquals("product-updated", foundEntity.getName());
+    }
+
+    @Test
+    public void duplicateError() {
+        ProductEntity duplicateProductEntity = new ProductEntity(savedEntity.getProductId(), "name", 1);
+        assertThrows(DuplicateKeyException.class, () -> {
+            productRepository.save(duplicateProductEntity);
+        });
     }
 
 }
