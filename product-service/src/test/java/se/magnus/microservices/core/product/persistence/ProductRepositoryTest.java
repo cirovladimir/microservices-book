5849b1d188da083f415279ab0db0f2bbb3be27a7
cirovladimir <cirovladimir@gmail.com>
2021-05-10 10:49:51 -0500

    add test for optimistic lock error

--- product-service/src/test/java/se/magnus/microservices/core/product/persistence/ProductRepositoryTest.java
+++ product-service/src/test/java/se/magnus/microservices/core/product/persistence/ProductRepositoryTest.java
@@ -5,12 +5,16 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.OptimisticLockingFailureException;
 
 @DataMongoTest
 public class ProductRepositoryTest {
+    private final Logger log = LoggerFactory.getLogger(ProductRepositoryTest.class);
 
     @Autowired
     ProductRepository productRepository;
@@ -52,6 +56,24 @@ public class ProductRepositoryTest {
         assertThrows(DuplicateKeyException.class, () -> {
             productRepository.save(duplicateProductEntity);
         });
+    }
+
+    @Test
+    public void optimisticLockError(){
+        ProductEntity productEntity1 = productRepository.findById(savedEntity.getId()).get();
+        ProductEntity productEntity2 = productRepository.findById(savedEntity.getId()).get();
+
+        productEntity1.setName("name-update-1");
+        productRepository.save(productEntity1);
+
+        productEntity2.setName("name-update-2");
+        assertThrows(OptimisticLockingFailureException.class, ()-> {
+            productRepository.save(productEntity2);
+        });
+
+        ProductEntity foundProductEntity = productRepository.findById(savedEntity.getId()).get();
+            assertEquals(1, foundProductEntity.getVersion());
+            assertEquals("name-update-1", foundProductEntity.getName());
     }
 
 }
