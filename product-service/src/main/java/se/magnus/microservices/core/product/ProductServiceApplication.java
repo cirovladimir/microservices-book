c11455a08ce9a2bb17b2ac847d1af03142228da1
cirovladimir <cirovladimir@gmail.com>
2021-05-11 00:17:17 -0500

    log info about mongo db instance

--- product-service/src/main/java/se/magnus/microservices/core/product/ProductServiceApplication.java
+++ product-service/src/main/java/se/magnus/microservices/core/product/ProductServiceApplication.java
@@ -1,15 +1,22 @@
 package se.magnus.microservices.core.product;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("se.magnus")
 public class ProductServiceApplication {
+	private final static Logger log = LoggerFactory.getLogger(ProductServiceApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(ProductServiceApplication.class, args);
+		String mongoHost = ctx.getEnvironment().getProperty("spring.data.mongodb.host");
+		String mongoPort = ctx.getEnvironment().getProperty("spring.data.mongodb.port");
+		log.info("Connected to MongoDB: {}:{}", mongoHost, mongoPort);
 	}
 
 }
